1521a0af69f0a88e1631e80192a0a5b23e231f8c
Nithish Reddy <nithishbanda2021@gmail.com>
2021-12-02 20:49:21 +0530

    Kush-Contests

--- src/queries/Contest.java
+++ src/queries/Contest.java
@@ -3,17 +3,247 @@ package queries;
 import java.sql.*;
 
 public class Contest {
-    public void createContests(Connection con){
-        try{
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from account");
-            while (rs.next()) {
-                System.out.println(rs.getString("account_number") + " " + rs.getDouble("balance"));
-            }
-            stmt.close();
-            con.close();
-        }catch (SQLException e){
-            e.printStackTrace();
+
+    public static void printTable(ResultSet rs) {
+        try {
+            System.out.println("+---------------+---------------+----------+--------------+--------------+");
+            System.out.println("|ContestID      |Author         |Division  |StartTime     |EndTime       |");
+            System.out.println("+---------------+---------------+----------+--------------+--------------+");
+            do {
+                System.out.printf("|%-15d|%-15s|%-10d|%-20s|%-20s|\n", rs.getString(1), rs.getString(2),
+                        rs.getInt(3),
+                        rs.getInt(4), rs.getString(5));
+            } while (rs.next());
+            System.out.println("+---------------+---------------+----------+--------------+--------------+");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
+
+    public static void displayAll(Connection con) {
+        try {
+            Statement st = con.createStatement();
+            ResultSet result1 = st.executeQuery("select * from contest");
+
+            if (result1.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                System.out.println("Contest Table :\n");
+                printTable(result1);
+            }
+            System.out.printf("\n");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void insertContest(Connection con, String tuple) {
+        try {
+            String[] args = tuple.split(" ");
+            String query = " insert into Contest(ContestID,Author,Division,StartTime,EndTime) values (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, Integer.parseInt(args[0]));
+            preparedStmt.setString(2, args[1]);
+            preparedStmt.setInt(3, Integer.parseInt(args[2]));
+            preparedStmt.setString(4, args[3]);
+            preparedStmt.setString(5, args[4]);
+            preparedStmt.execute();
+            System.out.println("Inserted successfully");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void updateAuthor(Connection con, String ContestID, String Author) {
+        try {
+            String query = "update Contest set Author = ? where ContestID = ? ";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, Integer.parseInt(ContestID));
+            preparedStmt.setString(2, Author);
+            int rs = preparedStmt.executeUpdate();
+            if (rs == 0) {
+                System.out.println("Update failed!!!");
+            } else {
+                System.out.println("Updated successfully");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void updateDivision(Connection con, String ContestID, String Division) {
+        try {
+            String query = "update Contest set Division = ? where ContestID = ? ";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, Integer.parseInt(Division));
+            preparedStmt.setInt(2, Integer.parseInt(ContestID));
+            int rs = preparedStmt.executeUpdate();
+            if (rs == 0) {
+                System.out.println("Update failed!!!");
+            } else {
+                System.out.println("Updated successfully");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void updateStartTime(Connection con, String ContestID, String StartTime) {
+        try {
+            String query = "update Contest set StartTime = ? where ContestID = ? ";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, StartTime);
+            preparedStmt.setInt(2, Integer.parseInt(ContestID));
+            int rs = preparedStmt.executeUpdate();
+            if (rs == 0) {
+                System.out.println("Update failed!!!");
+            } else {
+                System.out.println("Updated successfully");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void updateEndTime(Connection con, String ContestID, String EndTime) {
+        try {
+            String query = "update Contest set EndTime = ? where ContestID = ? ";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, EndTime);
+            preparedStmt.setInt(2, Integer.parseInt(ContestID));
+            int rs = preparedStmt.executeUpdate();
+            if (rs == 0) {
+                System.out.println("Update failed!!!");
+            } else {
+                System.out.println("Updated successfully");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void searchByContestId(Connection con, String ContestID) {
+        try {
+            String query = "select * from Contest where ContestID = ?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, Integer.parseInt(ContestID));
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                printTable(rs);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void searchByAuthor(Connection con, String Author) {
+        try {
+            String query = "select * from Contest where Author = ?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, Author);
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                printTable(rs);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void searchByDivision(Connection con, String Division, String operator) {
+        try {
+            String query = "select * from Contest where Division " + operator + " ?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, Integer.parseInt(Division));
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                printTable(rs);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void searchByStartTime(Connection con, String StartTime) {
+        try {
+            String query = "select * from Contest where StartTime = ?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, StartTime);
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                printTable(rs);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void searchByEndTime(Connection con, String EndTime) {
+        try {
+            String query = "select * from Contest where EndTime = ?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, EndTime);
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                printTable(rs);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void deleteContests(Connection con, String id) {
+        try {
+            String query = "delete from Contest where ContestID=?";
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, Integer.parseInt(id));
+            int rs = preparedStmt.executeUpdate();
+            if (rs == 0) {
+                System.out.println("Id " + id + " is not a registered Contest.");
+            } else {
+                System.out.println("Deleted");
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
+    public static void sortContests(Connection con, String sort) {
+        try {
+            String query = "select * from Contest order by " + sort;
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next() == false) {
+                System.out.println("No Result from Contest");
+            } else {
+                printTable(rs);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Wrong command\nType \"-h\" to get help");
+        }
+    }
+
 }
